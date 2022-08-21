e1452367dda3aa25336fa6246f55cb181d4184b2
Chris <chris.hoffmann84@gmail.com>
2022-08-21 21:12:52 +0200

    basic light line config

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -4,6 +4,10 @@ vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_functions = true
 
+vim.g.lightline = {
+  colorscheme = "tokyonight",
+}
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
